58312f5f05073b88d642814762962fec83255a22
Koen Zwikstra <>
2018-03-11 22:07:17 +0100

    adds libsodiumjni dependency for android

--- android/src/main/java/com/firstfloorsoftware/fluttersodium/FlutterSodiumPlugin.java
+++ android/src/main/java/com/firstfloorsoftware/fluttersodium/FlutterSodiumPlugin.java
@@ -5,6 +5,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import static org.libsodium.jni.NaCl.sodium;
 
 /**
  * FlutterSodiumPlugin
@@ -20,10 +21,23 @@ public class FlutterSodiumPlugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    try{
+      switch(call.method) {
+        case "sodium_version_string": sodium_version_string(call, result); break;
+
+        default: result.notImplemented(); break;
+      }
     }
+    catch(Exception error)
+    {
+      result.error("Error", call.method + " fails with " + error.getMessage(), null);
+    }
+  }
+
+  private void sodium_version_string(MethodCall call, Result result)
+  {
+    // FIXME: sodium().sodium_version_string() throws
+    // for now version is hardcoded
+    result.success("1.0.16");
   }
 }
