3c75e072836de4fe119e0dc06357440a90a34c40
Koen Zwikstra <>
2018-03-12 22:17:41 +0100

    fixes int const definitions

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,6 +1,5 @@
 const int crypto_shorthash_BYTES = crypto_shorthash_siphash24_BYTES;
-const crypto_shorthash_KEYBYTES = crypto_shorthash_siphash24_KEYBYTES;
-const String crypto_shorthash_PRIMITIVE = 'siphash24';
+const int crypto_shorthash_KEYBYTES = crypto_shorthash_siphash24_KEYBYTES;
 
 const int crypto_shorthash_siphash24_BYTES = 8;
 const int crypto_shorthash_siphash24_KEYBYTES = 16;
