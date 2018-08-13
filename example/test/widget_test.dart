78bdd49c02e9511e8c5aa142e34bb774e483042f
Koen Zwikstra <>
2018-08-14 00:44:03 +0200

    initial work on unittests

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,25 +1,63 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_sodium_example/main.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_sodium/flutter_sodium.dart';
+import 'dart:typed_data';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+  final List<MethodCall> log = <MethodCall>[];
+  const MethodChannel channel = MethodChannel('flutter_sodium');
+  channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    log.add(methodCall);
+  });
 
-    // Verify that platform version is retrieved.
-    expect(
-        find.byWidgetPredicate(
-          (Widget widget) =>
-              widget is Text && widget.data.startsWith('Running on:'),
-        ),
-        findsOneWidget);
+  tearDown(() {
+    log.clear();
+  });
+
+  group('CryptoAuth', () {
+    test('CryptoAuth.generateKey', () async {
+      await CryptoAuth.generateKey();
+      expect(
+          log, <Matcher>[isMethodCall('crypto_auth_keygen', arguments: null)]);
+    });
+
+    test('CryptoAuth.compute does not accept null value', () async {
+      expect(() async => await CryptoAuth.compute(null, null),
+          throwsAssertionError);
+    });
+    test('CryptoAuth.compute does not accept null key', () async {
+      expect(() async => await CryptoAuth.compute('hello world', null),
+          throwsAssertionError);
+    });
+
+    test('CryptoAuth.compute does not accept key of invalid length', () async {
+      expect(
+          () async => await CryptoAuth.compute('hello world', new Uint8List(1)),
+          throwsRangeError);
+    });
+  });
+
+  group('RandomBytes', () {
+    test('RandomBytes.buffer', () async {
+      await RandomBytes.buffer(16);
+      expect(log, <Matcher>[
+        isMethodCall('randombytes_buf',
+            arguments: <String, dynamic>{'size': 16})
+      ]);
+    });
+
+    test('RandomBytes.random', () async {
+      await RandomBytes.random();
+      expect(
+          log, <Matcher>[isMethodCall('randombytes_random', arguments: null)]);
+    });
+
+    test('RandomBytes.uniform', () async {
+      await RandomBytes.uniform(16);
+      expect(log, <Matcher>[
+        isMethodCall('randombytes_uniform',
+            arguments: <String, dynamic>{'upper_bound': 16})
+      ]);
+    });
   });
 }
