fcb0b522be8689348da36e435d1c9692a003046b
Koen Zwikstra <kozw@firstfloorsoftware.com>
2020-05-16 16:03:00 +0200

    adds support for loading libsodium on osx adds initial tests

--- test/flutter_sodium_test.dart
+++ test/flutter_sodium_test.dart
@@ -1,9 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_sodium/flutter_sodium.dart';
+import 'package:convert/convert.dart';
 
 void main() {
-  setUp(() {
-  });
+  setUp(() {});
 
-  tearDown(() {
+  tearDown(() {});
+
+  group('crypto_pwhash', () {
+    test('consts', () {
+      expect(Sodium.cryptoPwhashAlgArgon2i13, 1,
+          reason: 'cryptoPwhashAlgArgon2i13');
+      expect(Sodium.cryptoPwhashAlgArgon2id13, 2,
+          reason: 'cryptoPwhashAlgArgon2id13');
+      expect(Sodium.cryptoPwhashAlgDefault, 2,
+          reason: 'cryptoPwhashAlgDefault');
+    });
   });
 }
